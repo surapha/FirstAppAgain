4326a680115b8b9c1737674cc88307a616d541e7
USER PC <surapha.nu@rmuti.ac.th>
2020-08-24 22:25:15 +0700

    add build.gradle

---
+++ app/src/main/java/android/example/com/firstappagain/MainActivity.kt
@@ -0,0 +1,12 @@
+package android.example.com.firstappagain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
